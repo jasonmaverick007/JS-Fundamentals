4960afed2de8acbf61de15aea1e9d3f8a7d31a69
Jason <jasonretro580@gmail.com>
2025-05-29 09:52:06 +0300

    Removed .length in line 5

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,7 +2,7 @@ import { argv } from 'node:process';
 
 const args = argv.slice(2); // Skip 'node' and script path
 
-if (args.length === 0) {
+if (args == 0) {
   console.log('No argument');
 } else {
   args.forEach((val) => {
