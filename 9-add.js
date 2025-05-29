a3163225556d971b9b207aa4b7ba4d83de77acb5
Jason <jasonretro580@gmail.com>
2025-05-29 22:05:25 +0300

    changed line 8 to NaN

--- 9-add.js
+++ 9-add.js
@@ -5,7 +5,7 @@ function add(a, b) {
 const args = process.argv.slice(2);
 
 if (args.length !== 2 || isNaN(args[0]) || isNaN(args[1])) {
-  console.log("Usage: ./9-add.js <integer1> <integer2>");
+  console.log("NaN");
 } else {
   console.log(add(parseInt(args[0]), parseInt(args[1])));
 }
