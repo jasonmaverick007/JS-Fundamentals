1de24ef5466c82549b677b7180deee63ea56d580
Jason <jasonretro580@gmail.com>
2025-05-29 09:33:15 +0300

    Spelling correction in line 8

--- 2-arguments.js
+++ 2-arguments.js
@@ -5,7 +5,7 @@ const args = argv.slice(2);
 if (args.length === 0) {
   console.log('No argument');
 } else if(args.length === 1){
-    console.log('Agrument found');
+    console.log('Argument found');
 }else {
     console.log('Arguments found')
 }
