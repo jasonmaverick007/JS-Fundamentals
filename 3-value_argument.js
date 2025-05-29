ac1273f55067a2c37afa2203903a6fe47bafa36c
Jason <jasonretro580@gmail.com>
2025-05-29 10:29:11 +0300

    Added 3 in slice

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,9 @@
 const { argv } = require('node:process');
 
-const args = argv.slice(2); // Skip 'node' and script path
+const args = argv.slice(2, 3); // Skip 'node' and script path
 
-if (args == 0) {
-  console.log('No argument');
+if (args.length === 0) {
+  console.log('No arguments provided.');
 } else {
   args.forEach((val) => {
     console.log(`${val}`);
