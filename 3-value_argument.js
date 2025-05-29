82a6c52cd7963f32df68570e48f78a4550019d1b
Jason <jasonretro580@gmail.com>
2025-05-29 10:32:38 +0300

    Corrected errors found

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,8 +2,8 @@ const { argv } = require('node:process');
 
 const args = argv.slice(2, 3); // Skip 'node' and script path
 
-if (args.length === 0) {
-  console.log('No arguments provided.');
+if (args == 0) {
+  console.log('No argument');
 } else {
   args.forEach((val) => {
     console.log(`${val}`);
